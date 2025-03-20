b9fe26758aa8682b71aa2f2773078ed55bfb1b5c
Adeel Umar <adeelumar30@hotmail.com>
2025-03-21 00:58:58 +0500

    update particles.js

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,5 +1,5 @@
 // Load particles.js and apply configuration from particles.json
-particlesJS.load('particles-js', 'assets/particles.json', function () {
-    console.log('callback - particles.js config loaded');
-  });
-  
+particlesJS.load("particles-js", "assets/particles.json", () => {
+  console.log("callback - particles.js config loaded")
+})
+
